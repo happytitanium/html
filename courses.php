21bf1a1593ecfcc560817271952f5025ea27ee30
happytitanium <=>
2020-03-15 16:44:18 +0530

    medical section

--- courses.php
+++ courses.php
@@ -7,58 +7,63 @@ include 'includes/nav.php';
 ?>
 <br><br>
   <main id="main">
-    <section id="about">
+    <section id="services" class="section-bg">
       <div class="container">
 
         <header class="section-header">
-          <h3>Engineering</h3>
+          <h3>Courses</h3>
+
         </header>
 
-        <div class="row about-container">
+        <div class="row">
 
-          <div class="col-lg-6 content order-lg-1 order-2">
-            <div class="icon-box wow fadeInUp">
-              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
-              <h4 class="title"><a href="">About Exam </a></h4>
-              <p class="description">
-                JEE MAIN<br>
-                JEE Advanced <br>
-              </p>
+          <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-duration="1.4s" onclick="window.location.href='engineering.php'">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
+              <h4 class="title"><a href="engineering.php">Engineering</a></h4>
+              <p class="description"></p>
             </div>
-
-            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-photo"></i></div>
-              <h4 class="title"><a href="">List Of Colleges </a></h4>
-              <p class="description">Our experienced faculty and Counselling Experts give their advisory service to our students and guide them in registering for such exams.</p>
-            </div>
-
-            <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
-              <div class="icon"><i class="fa fa-bar-chart"></i></div>
-              <h4 class="title"><a href="">Synchronized Learning: </a></h4>
-              <p class="description">Complete synchronization of topics for Engineering Entrance Exams and Board Exams to avoid duplication of efforts.</p>
-            </div>
-            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-photo"></i></div>
-              <h4 class="title"><a href="">State of the Art Infrastructure: </a></h4>
-              <p class="description">Air – conditioned classrooms, small batches in classroom, innovative library facility, e – learning etc. all contribute to enriching learning experience</p>
-            </div>
-            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-photo"></i></div>
-              <h4 class="title"><a href="">Stress – Free Learning: </a></h4>
-              <p class="description"> Learning at GEENIUS is a stress free experience in flexible environment conducive to preparation for competitive exams. Individual support system makes students competent with their peer group, reducing their stress to a great extent.</p>
+          </div>
+          <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-duration="1.4s">
+            <div class="box" onclick="window.location.href='medical.php'">
+              <div class="icon"><i class="ion-ios-bookmarks-outline" style="color: #e9bf06;"></i></div>
+              <h4 class="title"><a href="medical.php">Medical</a></h4>
+              <p class="description"></p>
             </div>
           </div>
 
-          <div class="col-lg-6 background order-lg-2 order-1 wow fadeInUp">
-            <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+          <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s" onclick="window.location.href='foundation.php'">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-paper-outline" style="color: #3fcdc7;"></i></div>
+              <h4 class="title"><a href="foundation.php">Foundation</a></h4>
+              <p class="description"></p>
+            </div>
           </div>
+          <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s" onclick="window.location.href='dlp.php'">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-speedometer-outline" style="color:#41cf2e;"></i></div>
+              <h4 class="title"><a href="dlp.php">Distance Learning Program (DLP)</a></h4>
+              <p class="description"></p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s" onclick="window.location.href='sip.php'">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-world-outline" style="color: #d6ff22;"></i></div>
+              <h4 class="title"><a href="sip.php">School Integrated Program</a></h4>
+              <p class="description"></p>
+            </div>
+          </div>
+
+
         </div>
+
       </div>
-    </section><!-- End About Section -->
     </section><!-- End Services Section -->
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
+
 <?php
 include 'includes/footer.php';
  ?>
---
+++ medical.php
@@ -0,0 +1,360 @@
+<?php
+include 'includes/head.php';
+?>
+<body>
+>
+<?php
+include 'includes/nav.php';
+?>
+
+
+<br><br>
+  <main id="main">
+    <center>
+    <ul class="list-group" style="margin-top:40px;float:none;">
+      <li class="list-group-item">
+        <h1>About Exam</h1>
+          <p>
+            <ul>
+              <li><strong>NEET (UG)</strong></li>
+              <li><strong>Medical Council of India (MCI)</strong></li>
+              <li><strong>Dental Council of India (DCI)</strong></li>
+            </ul>
+          </p>
+      </li>
+      <li class="list-group-item">
+        <h1>List Of Colleges</h1>
+        <button type="button" data-toggle="modal" data-target="#collegeModal" name="button" class="btn btn-info">Click Here </button>
+      </li>
+      <li class="list-group-item">
+        <h1>Our Program</h1>
+        <section id="about">
+          <div class="container">
+
+            <header class="section-header">
+              <h3>Two year Classroom Course</h3>
+              <p>For Students moving to class XI</p>
+            </header>
+            <div class="row about-container">
+
+              <div class="col-lg-6 content order-lg-1 order-2">
+                <div class="icon-box wow fadeInUp">
+                  <div class="icon"><i class="fa fa-sticky-note"></i></div>
+                  <h4 class="title"><a href="">Course Details:</a></h4>
+                  <p class="description">Our Two Year Classroom Program is an ideal and most effective
+course for scoring a good rank in Medical Entrance Examinations, This course also
+simultaneously prepares a student in Physics, Chemistry &amp; Biology (PCB) for school
+and board examination. Besides building up strong fundamental concepts, the course
+also ensures sharpening of analytical thinking making students capable of solving
+application based complex &amp; tricky problems. The student is left with sufficient time
+for self study, removal of last minute doubt and building examination temperament.</p>
+                </div>
+
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+                  <div class="icon"><i class="fa fa-clock-o"></i></div>
+                  <h4 class="title"><a href="">Duration:</a></h4>
+                  <p class="description">More than 700 hrs. of exhaustive teaching comprising of basic training plus
+final revision (all Regular Practice Tests, Major Tests, Board Practice Tests, Online
+Tests, e – Learning and Doubt Clearing Sessions are extra).</p>
+                </div>
+
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                  <h4 class="title"><a href="">Study Material / Online</a></h4>
+                  <p class="description">An exhaustive and complete study material.<br>
+ GEENIUS Success File comprising of around 2000 objective questions.</p>
+                </div>
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-tags"></i></div>
+                  <h4 class="title"><a href="">Highlights</a></h4>
+                  <p class="description">
+                    Topic – wise board syllabus of class XI followed by Medical Entrance Exam <br>
+level training of the same topic will be completed by the end of January and
+syllabus of class XII board along with Medical Entrance Exam level training will
+be completed by the end of November. <br>
+ Regular Practice Test to review topics taught in class. <br>
+ Major Tests based on latest Medical Entrance Examinations pattern. <br>
+ Additional Board Practice Tests (BPT’s) for XII Board Exams. <br>
+ GEENIUS – All India Test Series, so that students can check their level of
+preparation and compare their rank at national level <br>
+ Regular feedback &amp; test analysis sessions. <br>
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-lg-6 background order-lg-2 order-1 wow fadeInUp">
+                <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+              </div>
+            </div>
+
+
+          </div>
+        </section>
+        <section id="about">
+          <div class="container">
+
+            <header class="section-header">
+              <h3>One Year classroom course</h3>
+              <p>For Students moving to class XII</p>
+            </header>
+            <div class="row about-container">
+              <div class="col-lg-6 background order-lg-1 order-1 wow fadeInUp">
+                <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+              </div>
+              <div class="col-lg-6 content order-lg-2 order-2">
+                <div class="icon-box wow fadeInUp">
+                  <div class="icon"><i class="fa fa-sticky-note"></i></div>
+                  <h4 class="title"><a href="">Course Details:</a></h4>
+                  <p class="description">Our One Year Classroom Program is specially deigned for preparing
+the students for Medical Entrance Examinations along with CBSE Board Examination.
+This course is designed to allow student enough time for self revision, removal of last
+minute doubts &amp; building examination temperament.</p>
+                </div>
+
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+                  <div class="icon"><i class="fa fa-clock-o"></i></div>
+                  <h4 class="title"><a href="">Duration:</a></h4>
+                  <p class="description">More than 500 hrs. of exhaustive teaching comprising of basic training plus
+final revision (all Regular Practice Tests, Major Tests, Board Practice Tests, Online
+Tests, e – Learning and Doubt Clearing Sessions are extra).</p>
+                </div>
+
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                  <h4 class="title"><a href="">Study Material / Online</a></h4>
+                  <p class="description">
+                    An exhaustive and complete study material.<br>
+                    GEENIUS Success File comprising of around 2000 objective questions.
+                  </p>
+                </div>
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-tags"></i></div>
+                  <h4 class="title"><a href="">Highlights</a></h4>
+                  <p class="description">
+                    Complete syllabus will be covered followed by final revision by the end of
+January. <br>
+ Regular Practice Tests to review topics taught in class.
+ Major Tests based on latest Medical entrance Examinations pattern. <br>
+ Additional Board Practice Tests (BPTs) for XII Board Exams. <br>
+ GEENIUS – All India Test Series, so that students can check their level of
+preparation and compare their rank at national level. <br>
+ Regular feedback &amp; test analysis sessions. <br>
+                  </p>
+                </div>
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-tags"></i></div>
+                  <h4 class="title"><a href="">Doubt Removing Sessions:</a></h4>
+                  <p class="description">
+                    In addition to the classroom teaching, students can
+discuss and clear their doubts from our faculty in Doubt Removing Sessions.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </section>
+        <section id="about">
+          <div class="container">
+
+            <header class="section-header">
+              <h3>One Year Classroom Course</h3>
+              <p>For XII passed students</p>
+            </header>
+            <div class="row about-container">
+
+              <div class="col-lg-6 content order-lg-1 order-2">
+                <div class="icon-box wow fadeInUp">
+                  <div class="icon"><i class="fa fa-sticky-note"></i></div>
+                  <h4 class="title"><a href="">Course Details:</a></h4>
+                  <p class="description">This course is for those who have not been able to make it to the Medical Entrance
+Examinations with a good rank and want to attempt again. It is a do or die situation
+for such students and even we on our part go all out to help such students realize
+their dreams by providing them extra individual attention and more strict discipline
+and testing norms. At this stage we need utmost sincerity and total devotion of the
+students so that we can make them achieve top ranks in Medical Entrance
+Examinations.</p>
+                </div>
+
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+                  <div class="icon"><i class="fa fa-clock-o"></i></div>
+                  <h4 class="title"><a href="">Duration:</a></h4>
+                  <p class="description">More than 500 hrs. of exhaustive teaching comprising of basic training plus final
+revision (all Regular Practice Tests, Major Test, Board Practice Tests, Online Tests, e –
+Learning and Doubt Clearing Sessions are extra).</p>
+                </div>
+
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-bars"></i></div>
+                  <h4 class="title"><a href="">Class Frequency:</a></h4>
+                  <p class="description">Classes are held 5 or 6 days a week for 4 or 5 hours per day.</p>
+                </div>
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                  <h4 class="title"><a href="">Study Material / Online</a></h4>
+                  <p class="description">An exhaustive and complete study material.<br>
+ GEENIUS Success File comprising of around 2000 objective questions.</p>
+                </div>
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-tags"></i></div>
+                  <h4 class="title"><a href="">Highlights</a></h4>
+                  <p class="description">
+                    Complete syllabus will be covered followed by final revision by the end of  January. <br>
+ Regular Practice Tests to review topics taught in class.<br>
+ Major Tests based on latest Engineering Entrance Examinations pattern.<br>
+ GEENIUS All India Test Series, so that students can check their level of
+preparation and compare their rank at national level.<br>
+ Regular feedback &amp; test analysis sessions.<br>
+                  </p>
+                </div>
+                <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+                  <div class="icon"><i class="fa fa-tags"></i></div>
+                  <h4 class="title"><a href="">Doubt Removing Sessions:</a></h4>
+                  <p class="description">
+                    In addition to the classroom teaching, students can
+discuss and clear their doubts from our faculty in Doubt Removing Sessions.
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-lg-6 background order-lg-2 order-1 wow fadeInUp">
+                <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="about">
+          <div class="container">
+
+            <header class="section-header">
+              <h3>Crash Course</h3>
+              <p>XII appearing / passed students</p>
+            </header>
+            <div class="row about-container">
+
+              <div class="col-lg-6 content order-lg-1 order-2">
+                <div class="icon-box wow fadeInUp">
+                  <div class="icon"><i class="fa fa-sticky-note"></i></div>
+                  <h4 class="title"><a href="">Course Details:</a></h4>
+                  <p class="description">
+                    This program helps students revise the entire course systematically and
+simultaneously trains them to tackle the entrance examination level problems in
+such a way that it helps them improve their expected All India Rank. <br>
+The entire syllabus is covered in the short period by the best professors and it is
+discussed from the entrance exam point of view. To give the students feel of the
+actual exams, they are also provided with All India Test Series through which they get
+to know their All India Rank across all GEENIUS centres. <br>
+Apart from this, few sessions on Time and Stress Management will also be provided
+to boost up the morale of students.
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-lg-6 background order-lg-2 order-1 wow fadeInUp">
+                <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="about">
+          <div class="container">
+
+            <header class="section-header">
+              <h3>All India Test Series (AITS)</h3>
+              <p>Online/Offline</p>
+            </header>
+            <div class="row about-container">
+
+              <div class="col-lg-6 content order-lg-1 order-2">
+                <div class="icon-box wow fadeInUp">
+                  <div class="icon"><i class="fa fa-sticky-note"></i></div>
+                  <h4 class="title"><a href="">Course Details:</a></h4>
+                  <p class="description">This course is meant for those students who have studied the topics themselves and
+want to check their preparation level and compete with Top Rankers on All India
+basis for Medical Entrance Exams.
+The students are prepared for the tests as per schedule provided to them at the time
+of admission. Test are both online &amp; offline.
+Students are provided with the detailed analysis of the test.
+Special doubt solving sessions are also provided to the deserving students depending
+upon their performance in these tests.</p>
+                </div>
+              </div>
+
+              <div class="col-lg-6 background order-lg-2 order-1 wow fadeInUp">
+                <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+              </div>
+            </div>
+
+
+          </div>
+        </section>
+      </li>
+    </ul>
+  </main><!-- End #main -->
+  <br>
+  <!-- Modal -->
+  <div id="collegeModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">List Of Colleges</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <p>Most Preferred Medical Colleges</p>
+<p>The most favoured Medical Colleges of India (Top 30) in order of</p>
+<p>preference</p>
+<p>1 All Indian Institute of Medical Sciences (AIIMS), Delhi</p>
+<p>2 Christian Medical College (CMC), Vellore</p>
+<p>3 Armed Forces Medical College (AFMC), Pune</p>
+<p>4 King George Medical College, Lucknow</p>
+<p>5 JIPMER, Pondicherry</p>
+<p>6 Kasturba Medical College, Manipal</p>
+<p>7 Maulana Azad Medical College, Delhi</p>
+<p>8 Grants Medical College (GMC), Mumbai</p>
+<p>9 Seth G.S. Medical College, Bangalore</p>
+<p>10 St John’s Medical College, Bangalore</p>
+<p>11 Madras Medical College, Chennai</p>
+<p>12 Institute of Medical Sciences, BHU, Varanasi</p>
+<p>13 L.T.M. Medical College, Sion, Mumbai</p>
+<p>14 PBDS PGIMS, Rohtak</p>
+<p>15 B.J. Medical College, Pune</p>
+<p>16 Amrit Institute of Medical Sciences, Kochi</p>
+<p>17 Bangalore Medical College, Bangalore</p>
+<p>18 Christian Medical College, Ludhiana</p>
+<p>19 Sri Ram Chandra Medical College, Chennai</p>
+<p>20 Osmania Medical College, Hyderabad</p>
+<p>21 Medical College &amp;amp; Hospital, Kolkata</p>
+<p>22 Stanley Medical College, Chennai</p>
+<p>23 Lady Hardinge Medical College, Delhi</p>
+<p>24 Kempegowda Institute of Medical Sciences, Bangalore</p>
+<p>25 Gandhi Medical College, Hyderabad</p>
+<p>26 Government Medical College, Bangalore</p>
+<p>27 MS Ramaiah Medical College, Bangalore</p>
+<p>28 Mahatma Gandhi Mission Medical College, Aurangabad</p>
+<p>29 Jawaharlal Nehru Medical College, Belgaum</p>
+<p>30 University College of Medical Sciences, Delhi</p>
+<p>LIST OF MBBS COLLEGES IN DELHI</p>
+<p>All India Institute of Medical Sciences</p>
+<p>Army College of Medical Science</p>
+<p>Lady Hardinge Medical College</p>
+<p>Maulana Azad Medical College &amp;amp; GB Pant Hospital</p>
+<p>Vardhman Mahavir Medical College &amp;amp; Safdarjung Hospital</p>
+<p>Hamdard Institute of Medical Sciences &amp;amp; Research</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======= Footer ======= -->
+<?php
+include 'includes/footer.php';
+ ?>
+
+</body>
+
+</html>
